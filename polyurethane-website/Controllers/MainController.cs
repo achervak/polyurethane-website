67821b4dc2b5492e56abce1eda414d84623bb119
gnylytsya <gnylytsya@gmail.com>
2017-07-16 16:51:08 +0300

    new project has been created

---
+++ polyurethane-website/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace polyurethane_website.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Default
+        public ActionResult Main()
+        {
+            ViewBag.Name = "Gogi";
+            return View();
+        }
+    }
+}
