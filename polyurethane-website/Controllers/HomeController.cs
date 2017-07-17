c994022dae7861278a8a14b68b8de1123ad20da3
DESKTOP-HBLP41N\Andrii <achervak>
2017-07-18 00:19:24 +0300

    // rebuild all | add VS soolution and base template

---
+++ polyurethane-website/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace polyurethane_website.Controllers
+{
+    public class HomeController : Controller
+    {       
+        public ActionResult Index()
+        {            
+            return View("Home");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        public ActionResult DeliveryDetails()
+        {
+            return View();
+        }
+        
+        public ActionResult Order()
+        {
+            return View();
+        }
+
+        public ActionResult Basket()
+        {
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+    }
+}
